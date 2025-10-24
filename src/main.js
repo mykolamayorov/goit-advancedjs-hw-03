1c0dfa037740efd5ffdb6655913c7a61ef2dd536
mykolamayorov <mykolamayorov@gmail.com>
2025-10-24 20:33:56 +0300

    submit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { fetchImages } from './js/pixabay-api.js';
-import { renderGallery, clearGallery } from './js/render-function.js';
+import { renderGallery, clearGallery } from './js/render-functions.js';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
