2e3c260a052fe1fc0b5dda914c5230a57aff4a84
mykolamayorov <mykolamayorov@gmail.com>
2025-10-24 21:28:59 +0300

    submit

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,16 +4,19 @@ const API_KEY = '52919144-2a7b1f23e18b84640824764ee';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export function fetchImages(query) {
-  const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(
-    query
-  )}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
 
-  return fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(data => data.hits);
+  return axios
+    .get(BASE_URL, { params })
+    .then(response => response.data.hits)
+    .catch(error => {
+      console.error('Axios error:', error);
+      throw error;
+    });
 }
